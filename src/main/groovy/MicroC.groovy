afafdaf095dcc842dcf3d883f7e486d23f95f51e
Jennifer Strater <jlstrater@users.noreply.github.com>
2016-10-28 12:18:57 +0200

    added checks for parse errors and file not found and added test to read all example files

--- src/main/groovy/MicroC.groovy
+++ src/main/groovy/MicroC.groovy
@@ -1,5 +1,6 @@
 import groovy.util.logging.Slf4j
 import org.antlr.v4.runtime.ANTLRFileStream
+import org.antlr.v4.runtime.BailErrorStrategy
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTree
 import org.antlr.v4.runtime.tree.ParseTreeWalker
@@ -13,12 +14,21 @@ class MicroC {
             return
         }
 
-        MicroCLexer lex = new MicroCLexer(new ANTLRFileStream(args[0]))
-        CommonTokenStream tokens = new CommonTokenStream(lex)
-        MicroCParser parser = new MicroCParser(tokens)
-        ParseTree tree = parser.program()
-        ParseTreeWalker walker = new ParseTreeWalker()
-        walker.walk( new MicroCWalker(), tree )
+        args.each {
+            log.info 'Starting Analysis for program: ' + it
+            try {
+                MicroCLexer lex = new MicroCLexer(new ANTLRFileStream(it))
+                CommonTokenStream tokens = new CommonTokenStream(lex)
+                MicroCParser parser = new MicroCParser(tokens)
+                parser.setErrorHandler(new BailErrorStrategy())
+                ParseTree tree = parser.program()
+                ParseTreeWalker walker = new ParseTreeWalker()
+                walker.walk(new MicroCWalker(), tree)
+            }
+            catch (FileNotFoundException ex) {
+                log.error "Error: File $it not found. Skipping..."
+            }
+        }
     }
 
 }
